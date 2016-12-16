c98b1efa9c1b4ba63572ac65e2ef3951cd63653e
Georges ARNOULD <georges.arnould@iprospect.com>
2016-12-16 13:56:47 +0100

    code cleaning (slightly)

--- lifecleaner.rb
+++ lifecleaner.rb
@@ -1,15 +1,25 @@
 #!/usr/bin/env ruby
 
+#####################
+### version
+
+VERSION = '1.0.2b'
+
+#####################
+# locate me (root receives script's directory)
+
+root = File.expand_path(File.dirname(__FILE__))
+
+#####################
+# requiring
+
 require 'twitter'
 require 'json'
 require 'date'
 require 'yaml'
 require 'getoptlong'
-
-#####################
-### version
-
-VERSION = '1.0.2'
+require "#{root}/lib/blank.rb"
+require "#{root}/lib/time.rb"
 
 #####################
 ### subs
@@ -53,156 +63,7 @@ def parseCommandLine
 
 end
 
-
 #####################
-### dirty imports
-
-# extending Time class
-
-class Time
-  def to_datetime
-    # Convert seconds + microseconds into a fractional number of seconds
-    seconds = sec + Rational(usec, 10**6)
-
-    # Convert a UTC offset measured in minutes to one measured in a
-    # fraction of a day.
-    offset = Rational(utc_offset, 60 * 60 * 24)
-    DateTime.new(year, month, day, hour, min, seconds, offset)
-  end
-end
-
-# taken from activesupport-4.2.5/lib/active_support/core_ext/object/blank.rb
-
-class Object
-  # An object is blank if it's false, empty, or a whitespace string.
-  # For example, +false+, '', '   ', +nil+, [], and {} are all blank.
-  #
-  # This simplifies
-  #
-  #   !address || address.empty?
-  #
-  # to
-  #
-  #   address.blank?
-  #
-  # @return [true, false]
-  def blank?
-    respond_to?(:empty?) ? !!empty? : !self
-  end
-
-  # An object is present if it's not blank.
-  #
-  # @return [true, false]
-  def present?
-    !blank?
-  end
-
-  # Returns the receiver if it's present otherwise returns +nil+.
-  # <tt>object.presence</tt> is equivalent to
-  #
-  #    object.present? ? object : nil
-  #
-  # For example, something like
-  #
-  #   state   = params[:state]   if params[:state].present?
-  #   country = params[:country] if params[:country].present?
-  #   region  = state || country || 'US'
-  #
-  # becomes
-  #
-  #   region = params[:state].presence || params[:country].presence || 'US'
-  #
-  # @return [Object]
-  def presence
-    self if present?
-  end
-end
-
-class NilClass
-  # +nil+ is blank:
-  #
-  #   nil.blank? # => true
-  #
-  # @return [true]
-  def blank?
-    true
-  end
-end
-
-class FalseClass
-  # +false+ is blank:
-  #
-  #   false.blank? # => true
-  #
-  # @return [true]
-  def blank?
-    true
-  end
-end
-
-class TrueClass
-  # +true+ is not blank:
-  #
-  #   true.blank? # => false
-  #
-  # @return [false]
-  def blank?
-    false
-  end
-end
-
-class Array
-  # An array is blank if it's empty:
-  #
-  #   [].blank?      # => true
-  #   [1,2,3].blank? # => false
-  #
-  # @return [true, false]
-  alias_method :blank?, :empty?
-end
-
-class Hash
-  # A hash is blank if it's empty:
-  #
-  #   {}.blank?                # => true
-  #   { key: 'value' }.blank?  # => false
-  #
-  # @return [true, false]
-  alias_method :blank?, :empty?
-end
-
-class String
-  BLANK_RE = /\A[[:space:]]*\z/
-
-  # A string is blank if it's empty or contains whitespaces only:
-  #
-  #   ''.blank?       # => true
-  #   '   '.blank?    # => true
-  #   "\t\n\r".blank? # => true
-  #   ' blah '.blank? # => false
-  #
-  # Unicode whitespace is supported:
-  #
-  #   "\u00a0".blank? # => true
-  #
-  # @return [true, false]
-  def blank?
-    BLANK_RE === self
-  end
-end
-
-class Numeric #:nodoc:
-  # No number is blank:
-  #
-  #   1.blank? # => false
-  #   0.blank? # => false
-  #
-  # @return [false]
-  def blank?
-    false
-  end
-end
-
 # main begins here ...
 
 puts "lifecleaner.rb"
